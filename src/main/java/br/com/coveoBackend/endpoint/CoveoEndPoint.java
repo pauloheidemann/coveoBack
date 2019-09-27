a5c9fe72a5e315b70fe473aeba1c17a18da0b6ed
Paulo Henrique Heidemann <e016486@celesc.corp>
2019-09-27 19:25:08 -0300

    implementing new endpoint to get all wines

--- src/main/java/br/com/coveoBackend/endpoint/CoveoEndPoint.java
+++ src/main/java/br/com/coveoBackend/endpoint/CoveoEndPoint.java
@@ -1,5 +1,7 @@
 package br.com.coveoBackend.endpoint;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
+import br.com.coveoBackend.service.CoveoService;
 import br.com.coveoBackend.service.ICoveoService;
 
 /**
@@ -29,16 +32,36 @@ public class CoveoEndPoint {
 	
 	@Autowired
 	private ICoveoService service;
+	
+	private static final Logger logger = LogManager.getLogger(CoveoEndPoint.class);
 
 	/**
 	 * Execute the query in the Coveo API using the HTTP method POST
 	 * @param data
 	 * @return
 	 */
-	@PostMapping(produces = "application/json", consumes = "application/json")
+	@PostMapping(value = "/all", produces = "application/json")
+	ResponseEntity<String> executePostQuery() {
+		try {
+			logger.info("Searching for all wines");
+			JsonNode result = service.postQuery("");
+			return new ResponseEntity<String>(result.toString(), HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<String>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+	/**
+	 * Execute the query in the Coveo API using the HTTP method POST
+	 * @param data
+	 * @return
+	 */
+	@PostMapping(produces = "application/json")
 	ResponseEntity<String> executePostQuery(@RequestBody String data) {
 		try {
+			logger.info("Searching for " + data);
 			JsonNode result = service.postQuery(data);
+			logger.info("Results are: " + result);
 			return new ResponseEntity<String>(result.toString(), HttpStatus.OK);
 		} catch (Exception e) {
 			return new ResponseEntity<String>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
@@ -50,9 +73,10 @@ public class CoveoEndPoint {
 	 * @param filter
 	 * @return
 	 */
-	@GetMapping(value = "{filter}", produces = "application/json", consumes = "application/json")
+	@GetMapping(value = "{filter}", produces = "application/json")
 	ResponseEntity<String> executeGetQuery(@PathVariable String filter) {
 		try {
+			logger.info("Searching for " + filter);
 			JsonNode result = service.getQuery(filter);
 			return new ResponseEntity<String>(result.toString(), HttpStatus.OK);
 		} catch (Exception e) {
